7b896c980538e9454debcf24e50de48448814793
Cristhian Traversi <crist.trav@gmail.com>
2023-03-23 12:25:22 -0300

    - Se agrego deteccion automatica de URL en produccion - Se cambio el spinner en el boton buscar votante

--- src/app/util/app-settings.ts
+++ src/app/util/app-settings.ts
@@ -1,5 +1,8 @@
+import { environment } from "src/environments/environment";
+
 export class AppSettings{
-    public static getURLAPI(): string{
-        return "http://localhost:3000/api";
+    public static getURLAPI(): string{        
+        if(!environment.production) return "http://localhost:3000/api";
+        return `${location.protocol}//${location.hostname}:${location.port}/api`;
     }
 }
